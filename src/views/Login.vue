103debad4934c32aa5586d906d28b3375fc7860e
Maria Jesus Fuenzalida Serrano <maria.fuenzalida.serrano@gmail.com>
2020-08-29 11:31:38 -0400

    Avance 3 con Login funcional

---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="login__container">
+    <login-form/>
+  </div>
+</template>
+
+<script>
+import LoginForm from '../components/LoginForm.vue'
+export default {
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style>
+.login__container {
+  margin: 0;
+  background-image: url(https://firebasestorage.googleapis.com/v0/b/usser-fe5a8.appspot.com/o/auto-6.jpg?alt=media&token=e27717a0-2f62-47e5-af7d-ce4a9a0f7c29) !important;
+  background-size: cover !important;
+  height: 100%;
+}
+</style>
