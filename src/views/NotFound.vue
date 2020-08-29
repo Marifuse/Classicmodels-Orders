2e470d39c442f70632010aa13114ee0e9029f856
Maria Jesus Fuenzalida Serrano <maria.fuenzalida.serrano@gmail.com>
2020-08-29 12:57:44 -0400

    Avance 7 con error 404 listo

---
+++ src/views/NotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="not__found">
+    <img class="error__404" src="error-404.jpg" alt="error-404">
+    <div class="text-center btn__nf">
+      <v-btn rounded color="grey" to="/home">🌟 Volver a Home 🌟</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.not__found {
+  background-color: #FFF;
+}
+.error__404 {
+  width: 20em;
+  display: block;
+  margin: auto;
+  margin-top: 3em;
+}
+.btn__nf {
+  margin-bottom: 11em;
+}
+</style>
