5206f6743219dd967086dc744223849316c36e10
Maria Jesus Fuenzalida Serrano <maria.fuenzalida.serrano@gmail.com>
2020-08-29 14:07:56 -0400

    Avance 10 con incorporación de algunas funciones al store de Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    currentUser: null,
   },
   mutations: {
+    // Mutación del usuario
+    SET_CURRENT_USER(state, user) { state.currentUser= user },
+    // Mutaciones overlay
+    SET_LOADING(state) { state.loading = true },
+    UNSET_LOADING(state) { state.loading = false },
   },
   actions: {
+    // Usser
+    setCurrentUser({commit}, user) { commit('SET_CURRENT_USER', user) },
   },
   modules: {
   }
